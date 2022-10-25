c3bb2a5e2726c4d701ca7a8e0a8913cc70e88aeb
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2022-10-25 12:08:01 +0100

    DLS-5704 upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,26 +3,23 @@ import sbt._
 
 object AppDependencies {
 
-  private val customsApiCommonVersion = "1.57.0"
-  private val scalaTestVersion = "3.2.9"
-  private val mockitoVersion = "3.11.2"
-  private val scalaTestPlusPlayVersion = "5.0.0"
-  private val wireMockVersion = "2.29.0"
   private val testScope = "test,it"
 
-  val compile = Seq("uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion withSources(),
-    "uk.gov.hmrc" %% "play-hmrc-api" % "6.4.0-play-28",
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full),
-    "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full)
+  val compile = Seq(
+    "uk.gov.hmrc"                                %% "customs-api-common" % "1.57.0" withSources(),
+    "uk.gov.hmrc"                                %% "play-hmrc-api"      % "7.0.0-play-28",
+    compilerPlugin("com.github.ghik" %  "silencer-plugin"    % "1.7.11" cross CrossVersion.full),
+    "com.github.ghik"                            %  "silencer-lib"       % "1.7.11" % Provided cross CrossVersion.full
+  )
 
   val test = Seq(
-    "org.scalatest" %% "scalatest" % scalaTestVersion % testScope,
-    "org.mockito" % "mockito-core" % mockitoVersion % testScope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % testScope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % testScope,
-    "com.github.tomakehurst" % "wiremock-jre8" % wireMockVersion % testScope,
-    "org.scalatestplus" %% "mockito-3-4" % "3.2.9.0" % testScope,
-    "com.vladsch.flexmark" %  "flexmark-all" % "0.36.8" % testScope,
-    "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion % testScope classifier "tests"
+    "org.scalatest"          %% "scalatest"          % "3.2.14"            % testScope,
+    "org.mockito"            %  "mockito-core"       % "4.8.1"             % testScope,
+    "com.typesafe.play"      %% "play-test"          % PlayVersion.current % testScope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"             % testScope,
+    "com.github.tomakehurst" %  "wiremock-jre8"      % "2.34.0"            % testScope,
+    "org.scalatestplus"      %% "mockito-3-4"        % "3.2.10.0"          % testScope,
+    "com.vladsch.flexmark"   %  "flexmark-all"       % "0.62.2"            % testScope,
+    "uk.gov.hmrc"            %% "customs-api-common" % "1.57.0"            % testScope classifier "tests"
     )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,19 +3,11 @@ resolvers += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/releases
 resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
 resolvers += Resolver.jcenterRepo
 
-
-addSbtPlugin("com.github.sbt" % "sbt-release" % "1.1.0")
-
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.8")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.1.0")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-stamp" % "6.2.0")
-
-addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.10.0-RC1")
-
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.8.2")
-
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.3.0")
+addSbtPlugin("com.github.sbt"    %  "sbt-release"           % "1.1.0")
+addSbtPlugin("com.typesafe.play" %  "sbt-plugin"            % "2.8.16")
+addSbtPlugin("uk.gov.hmrc"       %  "sbt-distributables"    % "2.1.0")
+addSbtPlugin("uk.gov.hmrc"       %  "sbt-git-stamp"         % "6.2.0")
+addSbtPlugin("net.virtual-void"  %  "sbt-dependency-graph"  % "0.10.0-RC1")
+addSbtPlugin("org.scoverage"     %  "sbt-scoverage"         % "1.8.2")
+addSbtPlugin("org.scalastyle"    %% "scalastyle-sbt-plugin" % "1.0.0")
+addSbtPlugin("uk.gov.hmrc"       %  "sbt-auto-build"        % "3.8.0")
