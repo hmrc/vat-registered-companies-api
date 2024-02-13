5acb82942c8abb3de617928385c382871cf4021b
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-02-13 15:19:22 +0000

    DLS-9914: Updates app dependencies - need to investigate play-hmrc-api for play30

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,19 +6,13 @@ object AppDependencies {
   private val testScope = "test,it"
 
   val compile = Seq(
-    "uk.gov.hmrc"                                %% "customs-api-common" % "1.60.0",
-    "uk.gov.hmrc"                                %% "play-hmrc-api"      % "7.2.0-play-28"
+    "uk.gov.hmrc" %% "customs-api-common" % "1.60.0",
+    "uk.gov.hmrc" %% "play-hmrc-api"      % "7.2.0-play-28"
   )
 
   val test = Seq(
-    "org.scalatest"          %% "scalatest"              % "3.2.16"            % testScope,
-    "org.mockito"            %  "mockito-core"           % "5.4.0"             % testScope,
-    "com.typesafe.play"      %% "play-test"              % "2.8.19" % testScope,
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"             % testScope,
-    "com.github.tomakehurst" %  "wiremock-jre8"          % "2.35.0"            % testScope,
-    "org.scalatestplus"      %% "mockito-3-4"            % "3.2.10.0"          % testScope,
-    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2"            % testScope,
-    "uk.gov.hmrc"            %% "customs-api-common"     % "1.60.0"            % testScope classifier "tests",
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "7.16.0"            % testScope
+    "uk.gov.hmrc"            %% "customs-api-common"     % "1.60.0" % testScope classifier "tests",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % "8.4.0"  % testScope,
+    "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.0"  % testScope
   )
 }
