09d90df2a892df851b192fb50abfded33afd2acf
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-02-15 14:15:53 +0000

    DLS-9914: Adds flexmark back

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,6 +10,7 @@ object AppDependencies {
   )
 
   val test = Seq(
+    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % testScope,
     "uk.gov.hmrc"            %% "customs-api-common"     % "1.60.0" % testScope classifier "tests",
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "8.4.0"  % testScope,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.0"  % testScope
