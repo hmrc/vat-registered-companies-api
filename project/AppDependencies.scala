7ebe31454898cdfc0d6755782cd6ebec22103e87
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-06-07 10:31:30 +0100

    DLS-10270: Reorders dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object AppDependencies {
 
-  private val playSuffix = "-play-30"
+  private val playVersion = "play-30"
 
   val compile = Seq(
-    "org.typelevel" %% "cats-core"                 % "2.9.0",
-    "uk.gov.hmrc"   %% s"play-hmrc-api$playSuffix" % "8.0.0"
+    "uk.gov.hmrc"   %% s"play-hmrc-api-$playVersion" % "8.0.0",
+    "org.typelevel" %% "cats-core"                   % "2.9.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% s"bootstrap-test$playSuffix" % "8.4.0",
-    "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.0"
+    "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion" % "8.4.0",
+    "org.scalatestplus.play" %% "scalatestplus-play"           % "7.0.0"
   ).map(_ % "test, it")
 }
